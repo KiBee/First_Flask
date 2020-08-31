6e2dd674cfb5a75e77a851b9ee1aaf285d8b6ae2
KiBee <killerbee1205@gmail.com>
2020-09-01 03:48:56 +1200

    First comm

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from collections import namedtuple
-
 from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost:3306/anime_norm_maria?charset=utf8'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost:3306/first_flask?charset=utf8'
 db = SQLAlchemy(app)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(1024), nullable=False)
+
+    def __init__(self, text, tags):
+        self.text = text.strip()
+        self.tags = [
+            Tag(text=tag.strip()) for tag in tags.split(',')
+        ]
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(32), nullable=False)
+
+    message_id = db.Column(db.Integer, db.ForeignKey('message.id'), nullable=False)
+    message = db.relationship('Message', backref=db.backref('tags', lazy=True))
+
+
+# db.create_all()
 
 
 @app.route('/', methods=['GET'])
@@ -15,16 +36,15 @@ def hello_world():
 
 @app.route('/main', methods=['GET'])
 def main():
-    return render_template('Main.html')
+    return render_template('main.html', messages=Message.query.all())
 
 
 @app.route('/add_message', methods=['POST'])
 def add_message():
     text = request.form['text']
     tag = request.form['tag']
-    # messages.append((Message(text, tag)))
+
+    db.session.add(Message(text, tag))
+    db.session.commit()
+
     return redirect(url_for('main'))
-
-
-if __name__ == '__main__':
-    app.run()
